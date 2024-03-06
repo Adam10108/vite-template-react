11f8b88a21928c9b6f6e4b1e6f6752612ecf8dda
Adam10108 <adam_atlas1998@hotmail.com>
2024-03-06 13:54:00 +0700

    Update `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+const serverPreviewSharedOptions = {
+  host: "0.0.0.0",
+  port: 3000,
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr(), tsconfigPaths()],
+  server: { ...serverPreviewSharedOptions },
+  preview: { ...serverPreviewSharedOptions },
 })
